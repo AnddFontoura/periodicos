930c9775d2a13a099e811d3bb7bd986869d990f5
Andre Fontoura <andd.fontoura@hotmail.com>
2021-07-17 14:34:56 -0300

    [Add] Category command to make fake data

--- app/Console/Commands/createCategoryDataForTest.php
+++ app/Console/Commands/createCategoryDataForTest.php
@@ -2,24 +2,25 @@
 
 namespace App\Console\Commands;
 
+use App\Category;
 use App\SubCategory;
 use Illuminate\Console\Command;
 
-class CreateSubCategoryDataForTest extends Command
+class createCategoryDataForTest extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'command:create-fake-subcategory-data {amount}';
+    protected $signature = 'command:create-fake-category-data {amount}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create fake data for subcategory';
+    protected $description = 'Create fake data for category';
 
     /**
      * Create a new command instance.
@@ -40,12 +41,22 @@ class CreateSubCategoryDataForTest extends Command
     {
         $amount = $this->argument('amount') ?? 1;
 
+        $category = Category::select('id')->get();
+
+        if (count($category) < 50) {
+            for ($i =0; $i < 50; $i++) {
+                Factory(Category::class)->create();
+            }
+        }
+
         $bar = $this->output->createProgressBar($amount);
 
         $bar->start();
 
         for ($i = 0; $i < $amount; $i++) {
-            Factory(SubCategory::class)->create();
+            Factory(SubCategory::class)->create([
+                'category_id' => SubCategory::inRandomOrder()->first()->id
+            ]);
             $bar->advance();
         }
 
