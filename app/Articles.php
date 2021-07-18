a03562cd2d08910a0102b8bf21dfb72a56f8850b
Andre Fontoura <andd.fontoura@hotmail.com>
2021-07-18 16:37:17 -0300

    [Add] Softdeletes at all models

--- app/Articles.php
+++ app/Articles.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Articles extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'path',
--- app/Category.php
+++ app/Category.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'subcategory_id',
         'name',
