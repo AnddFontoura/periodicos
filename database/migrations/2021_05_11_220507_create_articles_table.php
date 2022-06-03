966cea8f4cd11942fca2a6c22ddd7aeb69d400d4
André Fontoura <45948615+AnddFontoura@users.noreply.github.com>
2022-06-03 00:20:16 -0300

    [Bettering] migrations and view

--- database/migrations/2021_05_11_220507_create_articles_table.php
+++ database/migrations/2021_05_11_220507_create_articles_table.php
@@ -13,27 +13,22 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        try {
-            Schema::create('articles', function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->unsignedBigInteger('subcategory_id');
-                $table->string('name',200);
-                $table->string('path',1000);
-                $table->text('authors');
-                $table->text('resume');
-                $table->text('abstract');
-                $table->integer('pages')->default(0);
-                $table->text('keywords');
-                $table->text('image')->nullable(true);
-                $table->timestamps();
-                $table->softDeletes();
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('subcategory_id');
+            $table->string('name',200);
+            $table->string('path',1000);
+            $table->text('authors');
+            $table->text('resume');
+            $table->text('abstract');
+            $table->integer('pages')->default(0);
+            $table->text('keywords');
+            $table->text('image')->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
 
-                $table->foreign('subcategory_id')->references('id')->on('sub_categories');
-            });
-        } catch (Exception $e) {
-            Schema::dropIfExists('articles');
-            dd($e->getMessage());
-        }
+            $table->foreign('subcategory_id')->references('id')->on('sub_categories');
+        });
     }
 
     /**
